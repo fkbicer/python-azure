77fc017a00c36f8dd9792693fb829e517317d34e
furkankaan <fkbicer07@gmail.com>
2024-09-04 17:16:03 +0300

    test

--- main.py
+++ main.py
@@ -6,36 +6,41 @@ from azure_blob import get_blob_service_client
 
 app = FastAPI()
 
+
 class Category(Enum):
     TOOLS = "tools"
     CONSUMABLES = "consumables"
 
+
 class Item(BaseModel):
-    name :str
+    name: str
     price: float
     count: int
     id: int
     category: Category
 
+
 items = {
-    0: Item(name="Hammer", price=9.99, count=20,id=0,category=Category.TOOLS)
+    0: Item(name="Hammer", price=9.99, count=20, id=0, category=Category.TOOLS)
 }
 
 blob_service_client = get_blob_service_client()
-
 
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the FastAPI application!"}
 
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id, "description": "This is an item"}
 
+
 @app.get("/hello")
 def say_hello():
     return {"message": "Hello, World!"}
+
 
 @app.get("/containers")
 async def read_root():
